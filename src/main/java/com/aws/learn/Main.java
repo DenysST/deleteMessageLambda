ba98b2b3263fd0bfbb3706744c8fb8b8e5e71d6c
DenysST <85154693+DenysST@users.noreply.github.com>
2022-01-21 14:01:43 +0200

    little masks

--- src/main/java/com/aws/learn/Main.java
+++ src/main/java/com/aws/learn/Main.java
@@ -3,15 +3,14 @@ package com.aws.learn;
 import java.util.List;
 public class Main {
 
-    public void deleteExpiredMessages(int days) {
+    public String deleteExpiredMessages(int days) {
         MessageService service = new MessageService();
         List<Message> messages = service.getAll(days);
         if (messages.size() > 0 ) {
             service.deleteMessagesFromDb(messages);
             service.deleteMessageFromChime(messages);
-            System.out.println(messages.size() + " messages was deleted successes");
-            return;
+            return messages.size() + " messages was deleted successes";
         }
-        System.out.println("No expired messages");
+        return  "No expired messages";
     }
 }
